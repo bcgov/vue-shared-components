5df6fc160ee1699e9b484283a802a33f185d7ea3
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-08-25 10:41:37 -0700

    export callout

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import Footer from "./components/footer/Footer.vue";
 import Radio from "./components/radio/Radio.vue";
+import Callout from "./components/callout/Callout.vue";
 
 module.exports = {
     Footer,
     Radio,
+    Callout,
 };
