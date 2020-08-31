033fdc6d04f8987ccc26d219cdd60bcf787c4adf
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-08-31 16:18:03 -0700

    add textarea component

---
+++ src/components/textarea/Textarea.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div v-if="label" class="bcgov-input-label">
+      <label v-bind:for="id">{{ label }}</label>
+      {{ asterisk }}&nbsp;
+    </div>
+    <textarea
+      v-bind:id="id"
+      class="bcgov-text-input"
+      rows="8"
+      cols="60"
+      @change="$emit('textarea-change')"
+    />
+  </div>
+</template>
+
+<script>
+  import "./Textarea.css";
+
+  export default {
+    name: "Textarea",
+    props: {
+      isRequired: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+      id: {
+        type: String,
+        required: true,
+      },
+      label: {
+        type: String,
+        required: false,
+        default: "",
+      },
+    },
+    data() {
+      return {
+        asterisk: this.isRequired
+          ? <span id="asterisk" className="mandatory">*</span>
+          : ""
+      }
+    }
+  }
+</script>
