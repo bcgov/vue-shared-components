6dce642fbcc3ae5ee6f0a606513004075e1fe94c
Shreyas Devalapurkar <shreyas.devalapurkar@gov.bc.ca>
2020-09-09 11:04:34 -0700

    add more states to display box

--- src/components/display-box/DisplayBox.stories.js
+++ src/components/display-box/DisplayBox.stories.js
@@ -1,29 +1,11 @@
 import DisplayBox from "./DisplayBox.vue";
 import DisplayBoxTestData from "../../modules/DisplayBoxTestData.vue";
-import Table from "../table/Table.vue";
+import Account from 'mdi-vue/Account.vue';
 
 export default {
   title: 'DisplayBox',
   component: DisplayBox,
 };
-
-const elements = [
-  {
-    name: (
-      <p style={{ width: "80%" }}>
-        This is the content on the left of the display box. Maybe there will be
-        other content on the right too.
-      </p>
-    ),
-    value: (
-      <p style={{ width: "80%" }}>
-        This is the content on the right of the display box. There already is
-        content on the left side of the box.
-      </p>
-    ),
-    isSideBySide: true,
-  },
-];
 
 const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
@@ -34,4 +16,24 @@ const Template = (args, { argTypes }) => ({
 export const WithoutIcon = Template.bind({});
 WithoutIcon.args = {
   element: DisplayBoxTestData,
+};
+
+export const WithIcon = Template.bind({});
+WithIcon.args = {
+  icon: Account,
+  element: DisplayBoxTestData,
+};
+
+export const WithBlueBackground = Template.bind({});
+WithBlueBackground.args = {
+  icon: Account,
+  element: DisplayBoxTestData,
+  styling: "bcgov-blue-background",
+};
+
+export const WithBorder = Template.bind({});
+WithBorder.args = {
+  icon: Account,
+  element: DisplayBoxTestData,
+  styling: "bcgov-border-background",
 };
